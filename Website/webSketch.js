8b71e667663ca85ea4f54bc5ecf0ef672abe9bf6
NatalieCampos28 <74562485+NatalieCampos28@users.noreply.github.com>
2020-11-22 21:39:54 -0800

    Edited website files with new sketch edit.

---
+++ Website/webSketch.js
@@ -0,0 +1,33 @@
+//starting point
+var startX;
+    //movement
+var directionX = 5;
+
+function setup(){
+    //create canvas
+    let cnv = createCanvas(1000,100);
+    
+    //loads sketch for HTML and CSS to read
+    cnv.parent('sketch-holder');
+    
+    startX = width/2;
+}
+
+
+function draw(){
+    background(255,235,205);
+    
+    //adding movement
+     startX = startX + directionX;
+    
+    //restarts movement
+    if(startX>width){
+        startX = 200;
+    }
+    
+    fill(0);
+    textSize(30);
+    textFont('Georgia');
+    text("A Little Bit About Me!", startX, 65);
+    
+}
